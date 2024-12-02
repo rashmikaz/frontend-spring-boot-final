9f5ad4fe557714a8363953d6b6392f716acb56fe
rashmikaz <phrashmika@gmail.com>
2024-12-03 01:58:57 +0530

    add script.js

---
+++ frontend/js/script.js
@@ -0,0 +1,78 @@
+$("#login-section").css({display:'none'});
+$("#dashboard-section").css({display: 'block'});
+$("#field-section").css({display:'none'});
+$("#crop-section").css({display:'none'});
+$("#staff-section").css({display:'none'});
+$("#monitoring-section").css({display:'none'});
+$("#vehicle-section").css({display:'none'});
+$("#equipment-section").css({display:'none'});
+
+$('#nav-dashboard').on('click', () => {
+    $('#dashboard-section').css({display: 'block'});
+    $('#field-section').css({display: 'none'});
+    $('#crop-section').css({display: 'none'});
+    $("#staff-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
+    $("#vehicle-section").css({display:'none'});
+    $("#equipment-section").css({display:'none'});
+});
+
+$('#nav-field').on('click', () => {
+    $('#field-section').css({display: 'block'});
+    $('#dashboard-section').css({display: 'none'});
+    $('#crop-section').css({display: 'none'});
+    $("#staff-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
+    $("#vehicle-section").css({display:'none'});
+    $("#equipment-section").css({display:'none'});
+});
+
+$('#nav-crop').on('click', () => {
+    $('#crop-section').css({display: 'block'});
+    $('#dashboard-section').css({display: 'none'});
+    $('#field-section').css({display: 'none'});
+    $("#staff-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
+    $("#vehicle-section").css({display:'none'});
+    $("#equipment-section").css({display:'none'});
+});
+
+$('#nav-staff').on('click', () => {
+    $("#staff-section").css({display:'block'});
+    $('#crop-section').css({display: 'none'});
+    $('#dashboard-section').css({display: 'none'});
+    $('#field-section').css({display: 'none'});
+    $("#monitoring-section").css({display:'none'});
+    $("#vehicle-section").css({display:'none'});
+    $("#equipment-section").css({display:'none'});
+});
+
+$('#nav-monitor-log').on('click', () => {
+    $("#monitoring-section").css({display:'block'});
+    $("#staff-section").css({display:'none'});
+    $('#crop-section').css({display: 'none'});
+    $('#dashboard-section').css({display: 'none'});
+    $('#field-section').css({display: 'none'});
+    $("#vehicle-section").css({display:'none'});
+    $("#equipment-section").css({display:'none'});
+});
+
+$('#nav-vehicle').on('click', () => {
+    $("#vehicle-section").css({display:'block'});
+    $("#monitoring-section").css({display:'none'});
+    $("#staff-section").css({display:'none'});
+    $('#crop-section').css({display: 'none'});
+    $('#dashboard-section').css({display: 'none'});
+    $('#field-section').css({display: 'none'});
+    $("#equipment-section").css({display:'none'});
+});
+
+$('#nav-equipment').on('click', () => {
+    $("#equipment-section").css({display:'block'});
+    $("#vehicle-section").css({display:'none'});
+    $("#monitoring-section").css({display:'none'});
+    $("#staff-section").css({display:'none'});
+    $('#crop-section').css({display: 'none'});
+    $('#dashboard-section').css({display: 'none'});
+    $('#field-section').css({display: 'none'});
+});
