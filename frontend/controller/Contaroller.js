ace7fa9ad1987a0935f18d60e4f171ec9218529d
rashmikaz <phrashmika@gmail.com>
2024-12-03 20:00:50 +0530

    controller.js

---
+++ frontend/controller/Contaroller.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    fetchFieldNames("field_details");
+
+});
+
+
+function fetchFieldNames(targetElementId) {
+    $.ajax({
+        url: "http://localhost:8081/spring-boot-final/api/v1/field",
+        type: "GET",
+        contentType: "application/json",
+        success: function (response) {
+            console.log("Field name: ", response);
+
+            $(`#${targetElementId}`)
+                .empty()
+                .append($("<option>", { value: "", text: "Select Field" }));
+
+            response.forEach((field) => {
+                console.log(field);
+                $(`#${targetElementId}`).append(
+                    $("<option>", { value: field, text: field })
+                );
+            });
+        },
+        error: function (xhr, status, error) {
+            console.error("Error fetching field names:", status, error);
+        },
+    });
+}
